3d7c5f1f15cf169efc9c9fa4c89558aa629fc853
SabaPangani <spangani37@gmail.com>
2024-04-07 00:48:46 +0400

    update ui

--- src/components/modals/CategoryModal.tsx
+++ src/components/modals/CategoryModal.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { useService } from "../../hooks/useService";
 import { CategoryType, ServiceType } from "../../shared/ServiceType";
+import CancelSvg from "../svgs/CancelSvg";
 
 export default function CategoryModal({
   toggleModal,
@@ -17,18 +18,24 @@ export default function CategoryModal({
         onClick={toggleModal}
       ></div>
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20 bg-white p-5 rounded-md w-[600px]">
-        <p className="text-dark text-2xl font-medium mb-10">Select category</p>
+        <div className="flex flex-row justify-between items-center mb-10 border-border border-b pb-3">
+          <p className="text-dark text-xl font-bold">Select category</p>
+          <p onClick={toggleModal}>
+            <CancelSvg />
+          </p>
+        </div>
         <ul className="flex flex-col gap-y-5">
           {services.map((category) => (
             <li
               key={category.categoryId}
-              className="text-xl text-dark font-medium border-border border-b pb-1 px-5 cursor-pointer"
+              className="text-lg text-dark font-medium border-border border-b pb-5 px-5 cursor-pointer flex flex-row items-center gap-x-5"
               onClick={() => {
                 onSetCategory(category);
                 toggleModal();
               }}
             >
-              {category?.name}
+              <div className="rounded-full bg-cyan-600 w-6 h-6"></div>
+              <span>{category?.name}</span>
             </li>
           ))}
         </ul>
